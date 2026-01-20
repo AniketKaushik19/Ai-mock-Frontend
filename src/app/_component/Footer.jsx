01be445bf26e634f59be986c8b8e705de5dc67e9
Satyamchaubey9889 <Satyamchaubey9565@gmail.com>
2026-01-20 18:47:35 +0530

    6:47

---
+++ src/app/_component/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="border-t border-primary/20 py-10 text-center text-gray-400">
+            © 2026 InterviewAI. All rights reserved.
+        </footer>
+    );
+}
