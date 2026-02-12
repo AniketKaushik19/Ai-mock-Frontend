c3226f372b95f278989a7979864140d82fe09cb9
Gurusharan Singh <gurusharansingh2222@gmail.com>
2026-02-12 11:29:38 +0530

    proxy updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,17 +4,14 @@ import { cookies } from "next/headers";
 
 export async function getProfile() {
   try {
-    const cookieStore = await cookies(); // async in server actions
+    const cookieStore = await cookies();
     const token = cookieStore.get("token")?.value;
 
     if (!token) return null;
 
     const res = await fetch(`http://localhost:8082/api/v1/profile`, {
       headers: {
-        // If backend expects cookie:
         Cookie: `token=${token}`,
-        // Or if backend expects Authorization:
-        // Authorization: `Bearer ${token}`,
       },
       cache: "no-store",
     });
